56a5a6824caa7c456252cbf98ad1b7d582acb3e2
ahmadwael28 <ahmadwael28@gmail.com>
2022-02-09 17:34:39 +0200

    camera feature

--- src/infrastructure/navigation/settings.navigator.js
+++ src/infrastructure/navigation/settings.navigator.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { SettingsScreen } from "../../features/settings/screens/settings.screen";
 import { FavouritesScreen } from "../../features/settings/screens/favourites.screen";
+import { CameraScreen } from "../../features/settings/screens/camera.screen";
 import {
   createStackNavigator,
   CardStyleInterpolators,
@@ -13,6 +14,7 @@ export const SettingsNavigator = ({ route, navigation }) => {
     <SettingsStack.Navigator
       screenOptions={{
         cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+        headerShown: false,
       }}
     >
       <SettingsStack.Screen
@@ -23,6 +25,7 @@ export const SettingsNavigator = ({ route, navigation }) => {
         component={SettingsScreen}
       />
       <SettingsStack.Screen name="Favourites" component={FavouritesScreen} />
+      <SettingsStack.Screen name="Camera" component={CameraScreen} />
     </SettingsStack.Navigator>
   );
 };
